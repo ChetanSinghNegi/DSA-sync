65b65ee9f4cd00a492cc4b59dc5328f69690378e
Chetan Singh Negi <89253913+ChetanSinghNegi@users.noreply.github.com>
2026-07-31 18:09:53 +0530

    Time: 7 ms (46.66%) | Memory: 46.4 MB (98.76%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,27 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] nums2 = nums.clone();
-        Arrays.sort(nums2);
-        int i = 0, n = nums.length, j = n-1;
-        while(i<j){
-            int sum = nums2[i] + nums2[j];
-            if(sum == target) break;
-            else if(sum < target) i++;
-            else j--;
-        } 
-        int n1 = nums2[i], n2 = nums2[j];
-        for(int k = 0;k<n;k++){
-            if(nums[k] == n1) {
-                i = k;
-                break;
+        int n = nums.length;
+        HashMap<Integer, Integer> freq = new HashMap<>(); // num, freq
+        for(int i = 0;i<n;i++){
+            int num = nums[i];
+            int updatedFreq = freq.getOrDefault(num, 0)+1;
+            freq.put(num, updatedFreq);
+        }
+        for(int i = 0;i<n;i++){
+            int n1 = nums[i];
+            int diff = target - n1;
+            if(freq.containsKey(diff) == true){
+                if((diff == n1 && freq.get(diff) > 1) || diff != n1){
+                    for(int j = i+1;j<n;j++){
+                        if( nums[j] == diff) return new int[]{i,j};
+                    }
+                }
             }
         }
-        for(int k = n-1;k>=0;k--){
-            if(nums[k] == n2) {
-                j=k;break;
-            }
-        }
-
-        return new int[]{i,j};
+        return new int[2];
     }
 }
