e0f23d088c38c69f2100531afed7721661d9276a
Chetan Singh Negi <89253913+ChetanSinghNegi@users.noreply.github.com>
2026-08-10 18:13:29 +0530

    Time: 7 ms (69.22%) | Memory: 43.8 MB (93.27%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
-        int n1 = s1.length();
-        int n2 = s2.length();
-        for(int i = 0;i<n1;i++){
-            char ch = s1.charAt(i);
-            hm1.put(ch,hm1.getOrDefault(ch,0)+1);
+        int n1 = s1.length(), n2 = s2.length();
+        if (n1 > n2) return false;
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        // Build frequency for s1 and first window of s2
+        for (int i = 0; i < n1; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
         }
-        for(int i = 0;i<n1;i++){
-            char ch = s2.charAt(i);
-            hm2.put(ch,hm2.getOrDefault(ch,0)+1);
+
+        // Check initial window
+        if (matches(count1, count2)) return true;
+
+        // Slide the window
+        for (int i = n1; i < n2; i++) {
+            count2[s2.charAt(i) - 'a']++;              // add new char
+            count2[s2.charAt(i - n1) - 'a']--;        // remove old char
+            if (matches(count1, count2)) return true;
         }
-        if(hm1.equals(hm2)) return true;
-        for(int i = n1;i<n2;i++){
-            char ch1 = s2.charAt(i-n1);
-            if(hm2.get(ch1)==1) hm2.remove(ch1);
-            else hm2.put(ch1, hm2.get(ch1)-1);
-            char ch2 = s2.charAt(i);
-            hm2.put(ch2,hm2.getOrDefault(ch2,0)+1);
-            if(hm1.equals(hm2)) return true;
-        }
+
         return false;
+    }
+
+    private boolean matches(int[] count1, int[] count2) {
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) return false;
+        }
+        return true;
     }
 }
