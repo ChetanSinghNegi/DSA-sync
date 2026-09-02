9895842603f15d1722a6880ad186d1f82c780099
Chetan Singh Negi <89253913+ChetanSinghNegi@users.noreply.github.com>
2026-09-02 12:57:49 +0530

    Time: 53 ms (89.07%) | Memory: 282.4 MB (40.89%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,10 +1,8 @@
 class Solution {
     private class Graph{
-        int vertices;
         ArrayList<Integer>[] adjList;
 
         Graph(int vertices){
-            this.vertices = vertices;
             adjList = new ArrayList[vertices];
             for(int i = 0; i<vertices; i++){
                 adjList[i] = new ArrayList<>();
@@ -14,27 +12,42 @@ class Solution {
             adjList[src].add(des);
             adjList[des].add(src);
         }
+
+        public boolean dfs(int src, int des, boolean[] vis){
+            if(src == des) return true;
+            if(vis[src] == true) return false;
+            vis[src] = true;
+            for(int vertice: adjList[src]){
+                boolean isFound = dfs(vertice, des, vis);
+                if(isFound) return true;
+            }
+            return false;
+        }
+        public boolean bfs(int src, int des, boolean[] vis){
+            Queue<Integer> queue = new ArrayDeque<>();
+            queue.add(src);
+            while(queue.size()>0){
+                int top = queue.remove();
+                if(top == des) return true;
+                if(vis[top] == true) continue;
+                vis[top] = true;
+                for(int vertice: adjList[top]){
+                    if(vis[vertice] == false) queue.add(vertice);
+                }
+            }
+            return false;
+        }
     }
 
-    public boolean helper(int src, int des, Graph graph, HashSet<Integer> vis){
-        for(Integer vertice: graph.adjList[src]){
-            if(vis.contains(vertice)) continue;
-            if(vertice == des) return true;
-            vis.add(vertice);
-            boolean isFound = helper(vertice, des, graph, vis);
-            if(isFound) return true;
-        }
-        return false;
-    }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if(edges.length == 0) return true;
         Graph graph = new Graph(n);
         for(int[] edge: edges){
             graph.addEdge(edge[0],edge[1]);
         }
-        HashSet<Integer> vis = new HashSet<>();
-        boolean isFound = helper(source, destination, graph, vis);
+        boolean[] vis = new boolean[n];
+        // boolean isFound = graph.dfs(source, destination, vis);
+        boolean isFound = graph.bfs(source, destination, vis);
         return isFound;
     }
 }
