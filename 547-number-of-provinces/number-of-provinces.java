883f47f4cdf6dfb0ebf40207eadc20fe516a1863
Chetan Singh Negi <89253913+ChetanSinghNegi@users.noreply.github.com>
2026-09-03 21:03:42 +0530

    Time: 3 ms (26.50%) | Memory: 49.7 MB (9.11%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,39 @@
+class Solution {
+    class Graph{
+        ArrayList<ArrayList<Integer>> adjList;
+        Graph(int n){
+            adjList = new ArrayList<>();
+            for(int i = 0; i<n;i++){
+                adjList.add(new ArrayList<>());
+            }
+        }
+        public void addEdge(int src, int dest){
+            adjList.get(src).add(dest);
+        }
+        public void dfs(int src, boolean[] vis){
+            if(vis[src] == true) return;
+            vis[src] = true;
+            for(int vertice: adjList.get(src)){
+                if(vis[vertice] != true) dfs(vertice, vis);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int cnt = 0, n = isConnected.length;
+        Graph graph = new Graph(n);
+        for(int i = 0; i < n; i++){
+            int[] connection = isConnected[i];
+            for(int j = 0; j < n; j++){
+                if(i != j && connection[j] == 1) graph.addEdge(i,j);
+            }
+        }
+        boolean vis[] = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if(vis[i] == false){
+                cnt++;
+                graph.dfs(i, vis);
+            }
+        }
+        return cnt;
+    }
+}
